a63a849111697d3898caea62f79ee9194a562545
trinm31 <trinmgcd19833@fpt.edu.vn>
2022-01-15 13:41:32 +0700

    init project identity service

---
+++ Identity.Services/Identity.Services/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Identity.Services.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
