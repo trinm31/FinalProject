ab66abb24d08b0e7b092a8036d5bef958a7f4e8b
trinm31 <trinmgcd19833@fpt.edu.vn>
2022-03-07 13:39:16 +0700

    fix application user

---
+++ Identity.Services/Identity.Services/Migrations/20220306161835_AddmoreUserInfor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Identity.Services.Migrations
+{
+    public partial class AddmoreUserInfor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PersionalId",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PersionalId",
+                table: "AspNetUsers");
+        }
+    }
+}
--- Identity.Services/Identity.Services/Models/ApplicationUser.cs
+++ Identity.Services/Identity.Services/Models/ApplicationUser.cs
@@ -8,6 +8,7 @@ public class ApplicationUser : IdentityUser
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Position { get; set; }
+    public string PersionalId { get; set; }
     [NotMapped] 
     public string Role { get; set; }
 }
