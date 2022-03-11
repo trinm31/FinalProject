43d97d50b1e8404e10088d5c88558875384ea659
trinm31 <trinmgcd19833@fpt.edu.vn>
2022-03-11 14:20:48 +0700

    up date home page ui

--- ASP.NETCoreReact/ASP.NETCoreReact/ClientApp/src/components/Home.js
+++ ASP.NETCoreReact/ASP.NETCoreReact/ClientApp/src/components/Home.js
@@ -10,7 +10,7 @@ const Home = () =>{
                             <h1>Exam Scheduling App</h1>
                             <p>A fantastic app that allows you to divide the exam schedule and check in to the exam room using a QR code</p>
                             <div
-                                className="mt-2 lg:inline-flex lg:w-auto w-full px-3 py-2 rounded-lg bg-green-900 text-white items-center justify-center hover:bg-green-600 hover:text-white"
+                                className="mt-2 lg:inline-flex lg:w-auto w-full px-3 py-2 rounded-lg bg-grey text-white items-center justify-center hover:bg-gray-700 hover:text-white"
                             >
                                 <Link to="/student" className="hover:text-white">Student Check</Link>
                             </div>
